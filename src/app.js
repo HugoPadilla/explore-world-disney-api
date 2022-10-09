9de8ccc5b2787f06c96e111f812004d87726ebcf
Hugo Padilla <hugojavier19890@gmail.com>
2022-10-09 10:44:02 -0500

    build: configure morga dependency

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 import express from 'express'
+import morgan from 'morgan'
 import charactersRoutes from './routes/characters.routes.js'
 
 const app = express()
 
 // middlewares
 app.use(express.json())
+app.use(morgan('dev'))
 
 // routes
 app.use(charactersRoutes)
