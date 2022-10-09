2de7390a7dcbe5ef552fa0a843153c8cd7f98e10
Hugo Padilla <hugojavier19890@gmail.com>
2022-10-08 21:58:18 -0500

    fix: set properties as non-null and unique name for Character.js

--- src/models/Character.js
+++ src/models/Character.js
@@ -3,14 +3,30 @@ import { DataTypes } from 'sequelize'
 
 export const Character = sequelize.define('Character', {
   id: {
-    type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true, notNull: true
-  }, image: {
-    type: DataTypes.STRING, notNull: true
-  }, age: {
-    type: DataTypes.INTEGER, notNull: true
-  }, weight: {
-    type: DataTypes.STRING, notNull: true
-  }, story: {
-    type: DataTypes.STRING
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+    allowNull: false
+  },
+  image: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true
+  },
+  age: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  },
+  weight: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  story: {
+    type: DataTypes.STRING,
+    allowNull: false
   }
 })
